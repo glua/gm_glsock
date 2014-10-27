17479cec95e57a2aea74b2e46b7cdce3d2a61255
TrinityX <alexrsagen@gmail.com>
2014-10-27 19:38:25 +0100

    Changed build system to build.glua.me

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+dofile("../../common.lua")
+
+RequireDefaultlibs()
+
+SOLUTION        "gm_glsock"
+
+targetdir       "bins"
+INCLUDES        "source_sdk"
+INCLUDES        "gmod_sdk"
+defines         {"NDEBUG"}
+
+WINDOWS()
+LINUX()
+
+PROJECT()
+SOURCE_SDK_LINKS()
+configuration           "windows"
+configuration           "linux"
