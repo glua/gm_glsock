264969dba393910c3d2f2cc6f72adb0022f46887
Alexander Sagen <asagen99@gmail.com>
2014-11-14 19:58:13 +0100

    Fixed linux builds.

--- premake5.lua
+++ premake5.lua
@@ -9,9 +9,8 @@ INCLUDES        "source_sdk"
 INCLUDES        "gmod_sdk"
 defines         {"NDEBUG"}
 
-configuration "windows"
-  includedirs{"../../boost/includes"}
-  libdirs{"../../boost/libs"}
+includedirs{"../../boost-master"}
+libdirs{"../../boost-master/stage/lib"}
 
 WINDOWS()
 LINUX()
@@ -20,3 +19,35 @@ PROJECT()
 SOURCE_SDK_LINKS()
 configuration           "windows"
 configuration           "linux"
+  links{
+    "boost_atomic",
+    "boost_chrono",
+    "boost_context",
+    "boost_coroutine",
+    "boost_date_time",
+    "boost_filesystem",
+    "boost_graph",
+    "boost_locale",
+    "boost_log_setup",
+    "boost_log",
+    "boost_math_c99f",
+    "boost_math_c99l",
+    "boost_math_c99",
+    "boost_math_tr1f",
+    "boost_math_tr1l",
+    "boost_math_tr1",
+    "boost_prg_exec_monitor",
+    "boost_program_options",
+    "boost_random",
+    "boost_regex",
+    "boost_serialization",
+    "boost_signals",
+    "boost_sync",
+    "boost_system",
+    "boost_thread",
+    "boost_timer",
+    "boost_unit_test_framework",
+    "boost_wave",
+    "boost_wserialization",
+    "pthread",
+  }
